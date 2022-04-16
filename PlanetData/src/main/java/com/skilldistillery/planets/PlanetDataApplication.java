b9f17692954362d2eb591bdb3d4ae9dd71982d39
parisasledge-source <parisasledge@me.com>
2022-04-15 21:30:48 -0400

    second commit

---
+++ PlanetData/src/main/java/com/skilldistillery/planets/PlanetDataApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.planets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetDataApplication.class, args);
+	}
+
+}
